8c524371ddcd95637226f35a587940dbe5e124db
Jose Salgado Jr <jjsalga1@uci.edu>
2022-01-18 17:02:06 -0800

    Completed module 1 challenge.

---
+++ src/main/java/com/company/CourseGoals/Goals.java
@@ -0,0 +1,10 @@
+package com.company.CourseGoals;
+
+public class Goals {
+    public static void main(String[] args) {
+        System.out.println("The three goals I have for this class are:");
+        System.out.println("1. Master Java");
+        System.out.println("2. Master the git command line interface");
+        System.out.println("3. Create a good relationship with my mentor");
+    }
+}
